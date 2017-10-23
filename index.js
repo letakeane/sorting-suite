56b56f0e0fca5dc8cd84bcc2d2ac7653d56fa933
Leta Keane <letakeane+turing@gmail.com>
2017-10-23 13:52:21 -0600

    Package up module

---
+++ index.js
@@ -0,0 +1,11 @@
+const bubbleSort = require('./scripts/bubbleSort.js');
+const insertionSort = require('./scripts/insertionSort.js');
+const mergeSort = require('./scripts/mergeSort.js');
+const quickSort = require('./scripts/quickSort.js')
+
+module.exports = {
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort
+}
